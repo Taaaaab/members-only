9ffdeaee27550da4e2250767194731cbeee8d640
Taaaaab <owen.whitsett@gmail.com>
2023-01-19 14:22:32 -0500

    Added validation and sanitization methods for sign up form

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 require('dotenv').config()
+const { body, validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -18,8 +19,10 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const User = mongoose.model(
   "User",
   new Schema({
+    full_name: { type: String, required: true},
     username: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    membership_status: { type: Boolean, required: true}
   })
 );
 
@@ -75,21 +78,59 @@ app.get("/", (req, res) => {
     res.render("index", { user: req.user });
 });
 
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.get("/sign-up", (req, res) => {
+    res.render("sign-up-form", { errors: req.errors});
+});
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard", { user: req.user });
+});
 
 app.post("/sign-up", (req, res, next) => {
+    // Validate and sanitize fields.
+    body("full_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified."),
+    body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified."),
+    body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Password must be specified."),
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/errors messages.
+        res.render("sign-up-form", {
+          errors: errors.array(),
+        });
+        return;
+      }
+    }
+      // Data from form is valid.
+      // Create a User object with escaped and trimmed data.
     bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
         if (err) {
             return next(err);
         }
         const user = new User({
+            full_name: req.body.full_name,
             username: req.body.username,
-            password: hashedPassword
+            password: hashedPassword,
+            membership_status: false
           }).save(err => {
             if (err) { 
               return next(err);
             }
-            res.redirect("/");
+            res.redirect("/dashboard");
           });
       });
     
@@ -98,7 +139,7 @@ app.post("/sign-up", (req, res, next) => {
 app.post(
     "/log-in",
     passport.authenticate("local", {
-      successRedirect: "/",
+      successRedirect: "/dashboard",
       failureRedirect: "/"
     })
 );
